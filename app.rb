4490225ac60a491e64ec966d978d62f21f78fa71
Virginia Dooley <virginia@ideavine.org>
2014-06-30 12:14:44 -0400

    -a

--- app.rb
+++ app.rb
@@ -10,11 +10,12 @@ get '/' do
 	erb :home
 end
 
-# post '/sign-up' do
+# post '/sign-in' do
 #      puts “my params are” + params.inspect
 # end
 
-post '/sign-up' do
+#The following code *should* take the info entered in the form on home.erb and creat a user record in the database that can be used to sign in/out (create a session).
+post '/sign-in' do
      @user = User.where(username: params[:username]).first
      if @user.password == params[:password]
        redirect “/”
